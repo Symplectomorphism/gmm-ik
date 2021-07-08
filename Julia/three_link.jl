81fea4b080ebeab231c756f4f32d127621d933b2
Aykut C. Satici <acsatici@gmail.com>
2021-07-08 16:33:04 -0600

    EM converges, but no satisfactory answers.

--- Julia/three_link.jl
+++ Julia/three_link.jl
@@ -49,9 +49,32 @@ function ThreeLink(;N::Int=10)
     return robot
 end
 
+function ThreeLink(;N::Int=100, M::Int=10)
+    d = Uniform(-π, π)
+    θ = rand(d, 3, N)
+    x = rand(d, 2, N)
+    ξ = rand(d, 5, N)
+    # M = max(1, N ÷ 100)
+
+    for i = 1:N
+        fk!(x[:,i], θ[:,i])
+        ξ[:,i] = vcat(x[:,i], θ[:,i])
+    end
+
+    robot = ThreeLink(θ, x, ξ, M, N)
+    return robot
+end
+
 function fk!(x::Vector, θ::Vector)
     x[1] = cos(θ[1]) + cos(θ[1]+θ[2]) + 1/2*cos(sum(θ))
     x[2] = sin(θ[1]) + sin(θ[1]+θ[2]) + 1/2*sin(sum(θ))
+end
+
+function fk(θ::Vector)
+    x = zeros(2)
+    x[1] = cos(θ[1]) + cos(θ[1]+θ[2]) + 1/2*cos(sum(θ))
+    x[2] = sin(θ[1]) + sin(θ[1]+θ[2]) + 1/2*sin(sum(θ))
+    return x
 end
 
 function forward_kinematics!(r::ThreeLink, N::Int)
@@ -99,19 +122,45 @@ function _M_step(r::ThreeLink)
     end
 end
 
-function execute_EM(r::ThreeLink; maxiter::Int=100)
+function execute_EM(r::ThreeLink; maxiter::Int=10)
     for i = 1:maxiter
+        μ = zeros(5,r.M)
+        Σ = Array{Matrix{Float64}, 1}()
+        for i = 1:r.M
+            μ[:,i] = r.μ[:,i]
+            push!(Σ, r.Σ[i])
+        end
         _E_step(r)
         _M_step(r)
+        @info sum(norm(μ[:,i] - r.μ[:,i]) for i = 1:r.M)
+        @info sum(norm(Σ[i] - r.Σ[i]) for i = 1:r.M)
     end
 end
 
 function prediction(r::ThreeLink, x::Vector)
     μ_θ_tilde = zeros(3, r.M)
     Σ_θθ_tilde = Array{Matrix{Float64}, 1}()
+    d = Array{MvNormal, 1}()
+    β = zeros(r.M)
+    denominator = 0.0
+    θ_tilde = zeros(3)
+    Σ_θθ_tilde_final = zeros(3,3)
+
     for i = 1:r.M
         μ_θ_tilde[:,i] = r.μ[3:5,i] + r.Σ[i][3:5,1:2]*(r.Σ[i][1:2,1:2]\(x - r.μ[1:2,i]))
         push!(Σ_θθ_tilde, 
             r.Σ[i][3:5,3:5] - r.Σ[i][3:5,1:2]*(r.Σ[i][1:2,1:2]\r.Σ[i][1:2,3:5]))
+
+        Σ = r.Σ[i][1:2,1:2]
+        push!(d, MvNormal(r.μ[1:2,i], 1/2*(Σ + Σ')))
+        denominator += r.π[i] * pdf(d[i], x)
     end
+
+    for i = 1:r.M
+        β[i] = r.π[i] * pdf(d[i], x) / denominator
+        θ_tilde += β[i] * μ_θ_tilde[:,i]
+        Σ_θθ_tilde_final += β[i]*β[i]*Σ_θθ_tilde[i]
+    end
+
+    return θ_tilde, Σ_θθ_tilde_final
 end
