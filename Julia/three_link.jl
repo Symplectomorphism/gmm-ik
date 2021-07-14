a722c5f641d515151ae8387cfe30cd20cfe53668
Aykut C. Satici <acsatici@gmail.com>
2021-07-14 08:03:16 -0600

    update code

--- Julia/three_link.jl
+++ Julia/three_link.jl
@@ -13,6 +13,7 @@ using GaussianMixtures
 using PyCall, PyPlot
 using JuMP, Ipopt
 using BSON: @save, @load     # enable if you want to load one of the .bson files
+using CSV, DataFrames
 
 
 """
@@ -376,7 +377,7 @@ end
 function solve_optimization(x::Vector; start::Vector=rand(-π:0.1:π, 3))
     model = Model(Ipopt.Optimizer)
     @variable(model, θ[1:3])
-    @constraint(model, -π .<= θ .<= π)
+    @constraint(model, -2π .<= θ .<= 2π)
     @NLobjective(model, Min, 
         (cos(θ[1]) + cos(θ[1]+θ[2]) + 1/2*cos(θ[1]+θ[2]+θ[3]) - x[1])^2 + 
         (sin(θ[1]) + sin(θ[1]+θ[2]) + 1/2*sin(θ[1]+θ[2]+θ[3]) - x[2])^2
@@ -384,8 +385,19 @@ function solve_optimization(x::Vector; start::Vector=rand(-π:0.1:π, 3))
     set_start_value.(θ, start)
 
     JuMP.set_silent(model)
-    optimize!(model)
-    return value.(θ)
+
+    SUCCESS = [MOI.OPTIMAL, MOI.LOCALLY_SOLVED]
+    failure::Int=0
+    for i = 1:10
+        optimize!(model)
+        if any( termination_status(model) .== SUCCESS ) 
+            break
+        else
+            set_start_value.(all_variables(model), rand(-π:0.1:π, 3))
+        end
+    end
+    failure == 10 ? (@warn "IK solution may be wrong.") : nothing
+    return rem2pi.(value.(θ), RoundNearest), model
 end
 
 function solve_optimization(x::Vector, θ3::Float64; start::Vector=vcat(rand(-π:0.1:π, 2), θ3))
@@ -1214,4 +1226,154 @@ function move_ee_cs(r::ThreeLink; x0::Vector=zeros(2), xf::Vector=[-1.5, -0.4])
     # delete!(tl.vis)
     # close(tl.win)
     return tl
+end
+
+
+
+"""
+Data generation for assignment
+"""
+
+
+function jacobian!(J::Matrix, θ::AbstractArray)
+    a = [1., 1., 1/2]
+
+    J[1,1] = -a[1]*sin(θ[1]) - a[2]*sin(θ[1]+θ[2]) - a[3]*sin(θ[1]+θ[2]+θ[3])
+    J[1,2] = -a[2]*sin(θ[1]+θ[2]) - a[3]*sin(θ[1]+θ[2]+θ[3])
+    J[1,3] = -a[3]*sin(θ[1]+θ[2]+θ[3])
+    J[2,1] = a[1]*cos(θ[1]) + a[2]*cos(θ[1]+θ[2]) + a[3]*cos(θ[1]+θ[2]+θ[3])
+    J[2,2] = a[2]*cos(θ[1]+θ[2]) + a[3]*cos(θ[1]+θ[2]+θ[3])
+    J[2,3] = a[3]*cos(θ[1]+θ[2]+θ[3])
+end
+
+
+function construct_solution(;N::Int=11,seed::Int=0)
+    r = ThreeLink(N=N,M=1)
+    rng = MersenneTwister(seed)
+
+    d = Uniform(-1*π/180, 1*π/180)
+    θdot = rand(d, 3, N)
+    xdot = zeros(2, N)
+    J = zeros(2,3)
+    for i = 1:N
+        jacobian!(J, r.θ[:,i])
+        xdot[:,i] = J*θdot[:,i]
+    end
+
+
+    solution = DataFrame(
+        :x=>r.ξ[1,:],
+        :y=>r.ξ[2,:],
+        :th1=>r.ξ[3,:],
+        :th2=>r.ξ[4,:],
+        :th3=>r.ξ[5,:],
+        :xdot=>xdot[1,:],
+        :ydot=>xdot[2,:],
+        :th1dot=>θdot[1,:],
+        :th2dot=>θdot[2,:],
+        :th3dot=>θdot[3,:]
+    )
+    new_indices = randperm(rng, size(solution)[1])
+    for column in eachcol(solution)
+        column = column[new_indices]
+    end
+
+    return solution
+end
+
+function construct_forward_question(;N::Int=11, seed::Int=0)
+    question = construct_solution(N=N, seed=seed)
+    solution = deepcopy(question)
+    select!(question, Not([:x, :y, :xdot, :ydot]))
+    return question, solution
+end
+
+function construct_inverse_question(;N::Int=11, seed::Int=0)
+    question = construct_solution(N=N, seed=seed)
+    solution = deepcopy(question)
+    select!(question, Not([:th1, :th2, :th3, :th1dot, :th2dot, :th3dot]))
+    return question, solution
+end
+
+function generate_data(;nGroups=10)
+    str = "./data/group" 
+    
+    for i = 1:nGroups
+        if !isdir(str * string(i)) 
+            Base.Filesystem.mkdir(str * string(i))
+        else
+            Base.Filesystem.rm(str * string(i); recursive=true, force=true)
+            Base.Filesystem.mkdir(str * string(i))
+        end
+
+        question, solution = construct_forward_question(N=11, seed=i)
+        CSV.write(str * string(i) * "/fk_question.csv", question)
+        CSV.write(str * string(i) * "/fk_solution.csv", solution)
+
+        question, solution = construct_inverse_question(N=11, seed=i)
+        CSV.write(str * string(i) * "/ik_question.csv", question)
+        CSV.write(str * string(i) * "/ik_solution.csv", solution)
+    end
+    for n = nGroups+1:nGroups+50
+        if isdir(str * string(n))
+            Base.Filesystem.rm(str * string(n); recursive=true, force=true)
+        end
+    end
+end
+
+
+function sim_student()
+    str = "./submissions/group1"
+    data = CSV.File(str * "/ik_question.csv") |> DataFrame
+    N = size(data)[1]
+    th = zeros(3,size(data)[1])
+    thdot = zeros(3,size(data)[1])
+    J = zeros(2,3)
+
+    success::Int = 0
+    i = 1
+    for row in eachrow(data)
+        θ, model = solve_optimization(vcat(row[:x], row[:y]))
+        jacobian!(J, θ)
+        θdot = J \ vcat(row[:xdot], row[:ydot])
+        
+        if isapprox(fk(θ), vcat(row[:x], row[:y]); atol=1e-3) && isapprox(J*θdot, vcat(row[:xdot], row[:ydot]); atol=1e-3)
+            success += 1
+        end
+        th[:,i] = θ
+        thdot[:,i] = θdot
+        i += 1
+    end
+    @info "My success rate = $(round(success/N; digits=3)*100)%" 
+    my_solution = deepcopy(data)
+    for k = 1:3
+        insertcols!(my_solution, k+2, "th$(k)"=>th[k,:])
+        insertcols!(my_solution, size(my_solution)[2]+1, "th$(k)dot"=>thdot[k,:])
+    end
+
+    CSV.write(str * "/ik_solution.csv", my_solution)
+end
+
+function check_student()
+    str = "./submissions/group1"
+    str_sol = "./data/group1"
+    data = CSV.File(str_sol * "/ik_solution.csv") |> DataFrame
+    student_solution = CSV.File(str * "/ik_solution.csv") |> DataFrame
+    J = zeros(2,3)
+
+    success::Int = 0
+    i = 1
+    for row in eachrow(student_solution)
+        # θ_real = vcat(data[i,:][:th1], data[i,:][:th2], data[i,:][:th3])
+        θ_student = vcat(row[:th1], row[:th2], row[:th3])
+        θdot_student = vcat(row[:th1dot], row[:th2dot], row[:th3dot])
+        jacobian!(J, θ_student)
+
+        if isapprox(fk(θ_student), vcat(data[i,:][:x], data[i,:][:y]); atol=1e-3) && 
+            isapprox(J*θdot_student, vcat(row[:xdot], row[:ydot]); atol=1e-3)
+            success += 1
+        end
+        i += 1
+    end
+    @info "Student success rate = $(round(success/N; digits=3)*100)%" 
 end
